5a8f1a2db2aa20239c320c0f74bcc3c3a44f129b
varayoudfederico <varayoudfederico@gmail.com>
2022-02-09 15:52:19 -0300

    Entregable 9

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,50 +1,64 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons'
 
-const ItemCount = ({ stock, initial = 1, onAdd }) => {
+const ItemCount = ({
+  stock,
+  initial = 1,
+  onAdd,
+  onClose,
+  isAddModalVisible,
+}) => {
   const [selectedAmount, setSelectedAmount] = useState(initial)
 
-  return (
-    <div className="flex flex-col justify-between w-1/2 gap-4 py-4 rounded-lg bg-cardBackground">
-      <div className="flex items-center gap-2">
-        <span className="text-gray-500">{stock} disponibles</span>
-      </div>
-      {stock > 0 ? (
-        <div className="flex items-center w-full gap-4">
-          <span>Cantidad: </span>
-          <div className="flex items-center select-none">
-            <div
-              onClick={() =>
-                selectedAmount > 1 && setSelectedAmount(selectedAmount - 1)
-              }
-              className="flex items-center justify-center w-8 h-8 mr-4 text-white rounded-full cursor-pointer bg-primary"
-            >
-              <FontAwesomeIcon icon={faMinus} />
-            </div>
-            <p className="text-xl font-bold">{selectedAmount}</p>
-            <div
-              onClick={() => {
-                selectedAmount < stock && setSelectedAmount(selectedAmount + 1)
-              }}
-              className="flex items-center justify-center w-8 h-8 ml-4 text-white rounded-full cursor-pointer bg-primary"
-            >
-              <FontAwesomeIcon icon={faPlus} />
+  return isAddModalVisible ? (
+    <div
+      onClick={onClose}
+      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75"
+    >
+      <div className="flex flex-col justify-between w-1/2 gap-4 p-6 rounded-lg bg-cardBackground">
+        <div className="flex items-center gap-2">
+          <span className="text-gray-500">{stock} disponibles</span>
+        </div>
+        {stock > 0 ? (
+          <div className="flex items-center w-full gap-4">
+            <span>Cantidad: </span>
+            <div className="flex items-center select-none">
+              <div
+                onClick={(e) => {
+                  e.stopPropagation()
+                  selectedAmount > 1 && setSelectedAmount(selectedAmount - 1)
+                }}
+                className="flex items-center justify-center w-8 h-8 mr-4 text-white rounded-full cursor-pointer bg-primary"
+              >
+                <FontAwesomeIcon icon={faMinus} />
+              </div>
+              <p className="text-xl font-bold">{selectedAmount}</p>
+              <div
+                onClick={(e) => {
+                  e.stopPropagation()
+                  selectedAmount < stock &&
+                    setSelectedAmount(selectedAmount + 1)
+                }}
+                className="flex items-center justify-center w-8 h-8 ml-4 text-white rounded-full cursor-pointer bg-primary"
+              >
+                <FontAwesomeIcon icon={faPlus} />
+              </div>
             </div>
           </div>
-        </div>
-      ) : (
-        <span>El producto no tiene stock</span>
-      )}
-      <button
-        disabled={stock < 1}
-        onClick={() => stock > 0 && onAdd(selectedAmount)}
-        className="p-2 font-bold text-white rounded-lg cursor-pointer bg-primary disabled:bg-gray-600"
-      >
-        Agregar
-      </button>
+        ) : (
+          <span>El producto no tiene stock</span>
+        )}
+        <button
+          disabled={stock < 1}
+          onClick={() => stock > 0 && onAdd(selectedAmount)}
+          className="p-2 font-bold text-white rounded-lg cursor-pointer bg-primary disabled:bg-gray-600"
+        >
+          Agregar
+        </button>
+      </div>
     </div>
-  )
+  ) : null
 }
 
 export default ItemCount
--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,26 +1,60 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import ItemCount from './ItemCount'
 import { getCategoryName } from '../utils/categoryUtils'
 
 const ItemDetail = ({ item }) => {
   const { id, name, description, stock, category } = item
+  const [isAddModalVisible, setIsAddModalVisible] = useState(false)
+  const [selectedAmount, setSelectedAmount] = useState(0)
+
   return (
-    <div>
-      <div className="p-4 bg-cardBackground rounded-xl">
-        <p>
-          <b>ID:</b> {id}
-        </p>
-        <p>
-          <b>Nombre:</b> {name}
-        </p>
-        <p>
-          <b>Descripción:</b> {description}
-        </p>
-        <p>
-          <b>Categoria:</b> {getCategoryName(category)}
-        </p>
-        <ItemCount stock={stock} />
+    <>
+      <ItemCount
+        stock={stock}
+        isAddModalVisible={isAddModalVisible}
+        onAdd={(selectedAmount) => {
+          setSelectedAmount(selectedAmount)
+          setIsAddModalVisible(false)
+        }}
+        onClose={() => {
+          setIsAddModalVisible(false)
+        }}
+      />
+      <div>
+        <div className="p-4 bg-cardBackground rounded-xl">
+          <p>
+            <b>ID:</b> {id}
+          </p>
+          <p>
+            <b>Nombre:</b> {name}
+          </p>
+          <p>
+            <b>Descripción:</b> {description}
+          </p>
+          <p>
+            <b>Categoria:</b> {getCategoryName(category)}
+          </p>
+          <div className="flex gap-3 py-4">
+            <button
+              onClick={() => {
+                setIsAddModalVisible(true)
+              }}
+              className="p-2 font-bold text-white rounded-lg cursor-pointer bg-primary disabled:bg-gray-600"
+            >
+              {selectedAmount ? 'Editar cantidad' : 'Agregar al carrito'}
+            </button>
+            {selectedAmount ? (
+              <Link to="/cart">
+                <button className="p-2 font-bold text-white rounded-lg cursor-pointer bg-primary disabled:bg-gray-600">
+                  Terminar compra
+                </button>
+              </Link>
+            ) : null}
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
